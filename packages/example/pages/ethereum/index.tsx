d60035ec7efd9a0f104800fd106ca0e6fc2ffbee
Hopsken <shaowei5@icloud.com>
2022-05-06 20:32:43 +0800

    refact: evm test page

---
+++ packages/example/pages/ethereum/index.tsx
@@ -0,0 +1,19 @@
+import dynamic from 'next/dynamic';
+import styles from '../../styles/Home.module.css';
+import Link from 'next/link';
+import { LogsContainer } from '../../components/LogsContainer';
+
+// near provider works only if nextjs ssr disabled
+const EVMExample = dynamic(() => import('../../components/ethereum/EVMExample'), { ssr: false });
+
+export default function () {
+  return (
+    <div>
+      <div className={styles.container}>
+        <Link href={'/'}>← Back</Link>
+        <EVMExample />
+      </div>
+      <LogsContainer />
+    </div>
+  );
+}
